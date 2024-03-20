c7d71df2bb2103e3799eb4f8ee991ec635fa8ea7
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-20 17:26:30 -0400

    search bar well functional

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { searchQuery, feedQuery } from '../utils/data'
+import { client } from '../utils/client'
+import Spinner from './Spinner'
+import MasonryLayout from './MasonryLayout'
 
-const Search = () => {
+const Search = ({searchTerm}) => {
+  const[pins, setPins] = useState();
+  const[loading, setLoading] = useState(false);
+
+  useEffect(()=>{
+    if(searchTerm !== ''){
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data)=>{
+        setPins(data);
+        setLoading(false);
+      })
+
+    }else{
+      client.fetch(feedQuery).then((data)=>{
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  }, [searchTerm]);
+
   return (
-    <div>Search</div>
+    <div>
+      {loading &&(<Spinner message="Searching for the Pins"/>)};
+      {pins?.length !== 0 && (<MasonryLayout pins={pins}/>)}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className='mt-10 text-center text-xl'>
+          No Any Pins Found
+        </div>
+      )};
+    </div>
   )
 }
 
