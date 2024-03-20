eb0d01390f1e47618c54c86ad08693aa73eb4af5
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-20 16:35:36 -0400

    createPin comp well designed

--- src/components/CreatePin.js
+++ src/components/CreatePin.js
@@ -65,50 +65,56 @@ const CreatePin = ({user}) => {
     }
   };
   return (
-    <div className=''>
-      <p>Please Add all Fields</p>
-      <div>
-        {loading && (<Spinner/>)}{wrongImageType && (<p>It&apos;s wrong file type</p>)}
-        {!imageAsset ? (
-          <label>
-            <div>
+    <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
+      {fields && (
+        <p>Please Add all Fields</p>
+      )}
+      <div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
+        <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
+          <div className='flex flex-col justify-center items-center border-2 border-dotted border-gray-300 p-3 w-full h-420'>
+            {loading && (<Spinner/>)}{wrongImageType && (<p>It&apos;s wrong file type</p>)}
+            {!imageAsset ? (
+              <label>
+                <div className='flex flex-col items-center justify-center h-full'>
+                  <div className=' flex flex-col items-center justify-center'>
+                    <p><AiOutlineCloudUpload/></p>
+                    <p>Click to upload</p>
+                  </div>
+                  <p>Recommendation: Please use high quality image</p>
+                </div>
+                <input
+                  type='file'
+                  name='upload image'
+                  onChange={uploadImage}
+                  className='w-0 h-0'
+                />
+              </label>  
+            ) : (
               <div>
-                <p><AiOutlineCloudUpload/></p>
-                <p>Click to upload</p>
+                <img
+                  src={imageAsset?.url}
+                  alt="uploaded-pic"
+                  className='h-full w-full'
+                />
+                <button type='button' onClick={()=>{setImageAsset(null)}} className='absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out'>
+                  <MdDelete/>
+                </button>
               </div>
-              <p>Recommendation: Please use high quality image</p>
-            </div>
-            <input
-              type='file'
-              name='upload image'
-              onChange={uploadImage}
-              className=''
-            />
-          </label>  
-        ) : (
-          <div>
-            <img
-              src="https://react-icons.github.io/react-icons/search/#q=AiTwotoneDelete"
-              alt="uploaded-pic"
-            />
-            <button type='button' onClick={()=>{setImageAsset(null)}}>
-              <MdDelete/>
-            </button>
+            )}
           </div>
-        )}
-
-        <div>
+        </div>
+        <div className='flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full'>
           <input
             type='text'
             value={title}
             onChange={(e)=>setTitle(e.target.value)}
             placeholder='add your title'
-            className=''
+            className='outline-none text-2xl sm-text-3xxl font-bold border-b-2 border-gray-200 p-2'
           />
           {user && (
             <div>
-              <img src={user.image} alt='user-profile'/>
-              <p>{user.userName}</p>
+              <img src={user.image} alt='user-profile' className='w-10 h-10 rounded-full'/>
+              <p className='font-bold'>{user.userName}</p>
             </div>
           )}
 
@@ -117,7 +123,7 @@ const CreatePin = ({user}) => {
             value={about}
             placeholder='about ...'
             onChange={(e)=>setAbout(e.target.value)}
-            className=''
+            className='outline-none text-base sm:text-lg border-b-2 border-gray 200 p-2'
 
           />
 
@@ -126,31 +132,33 @@ const CreatePin = ({user}) => {
             placeholder='destination'
             value={destination}
             onChange={(e)=>setDestination(e.target.value)}
-            className=''
+            className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
           />
-          <div>
-            <p>Choose Your Pin Category</p>
-            <select
-              onChange={(e)=>{
-                setCategory(e.target.value);
-              }}
-            >
-              <option>
-                select categories
-              </option>
-              {categories.map((catg)=>(
-                <option>{catg.name}</option>
-              ))}
-            </select>
+          <div className='flex flex-col'>
+            <div className=''>
+              <p className='mb-2 font-semibold text:lg sm:text-xl'>Choose Your Pin Category</p>
+              <select
+                onChange={(e)=>{
+                  setCategory(e.target.value);
+                }}
+                className='outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer'
+              >
+                <option value="others" className='sm:text-bg bg-white'>
+                  select categories
+                </option>
+                {categories.map((catg)=>(
+                  <option className='text-base border-0 outline-none capitalize bg-white text-black'>{catg.name}</option>
+                ))}
+              </select>
+            </div>
+            <div className='flex justify-end items-end mt-5'>
+              <button type='button' onClick={savePin} className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none'>
+                  Save Pin
+              </button>
+            </div>
           </div>
-
-          <div className='flex justify-end items-end mt-5'>
-            <button type='button' onClick={savePin} className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none'>
-                Save Pin
-            </button>
-          </div>
-          
         </div>
+        
       </div>
     </div>
   )
