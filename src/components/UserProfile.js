d9d9f1e53c3a168577962bb173b60a107f85bfae
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-20 20:15:43 -0400

    userProfile creating

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+const activeBtnStyles = "bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none";
+const notActiveBtnStyles = "bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none";
 const UserProfile = () => {
   return (
     <div>UserProfile</div>
